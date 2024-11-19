9326fa5b91ef0a93c718a574090b9a5933151e42
Abe <field.abe.d@gmail.com>
2024-11-18 21:28:03 -0700

    Made login placeholder

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import '../styles/login.css';
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
-      <p>Please log in to continue.</p>
+    <div className="login-container">
+      <div className="login-card">
+        <h1 className="login-title">Login</h1>
+        <p className="login-description">Welcome back! Please log in to continue.</p>
+        <form className="login-form">
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" placeholder="Enter your username" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" placeholder="Enter your password" />
+          </div>
+          <button type="submit" className="login-button">Login</button>
+        </form>
+        <div className="login-links">
+          <a href="/forgot-password" className="forgot-password-link">Forgot Password?</a>
+          <a href="/sign-up" className="sign-up-link">Sign Up</a>
+        </div>
+      </div>
     </div>
   );
 };
